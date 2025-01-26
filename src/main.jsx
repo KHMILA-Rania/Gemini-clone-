9ef98a7898089f6111bcb9cff400486e63c5028f
Rania KHMILA <rania.khmila@gmail.com>
2025-01-26 19:41:05 +0100

    Revert " almost there" 9029204e91b4d4fbed4dbcd00943f621ff4ee3a5 This reverts commit 9029204e91b4d4fbed4dbcd00943f621ff4ee3a5.
    solved conflict

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ContextProvider from './context/Context.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <ContextProvider>
+  <StrictMode>
     <App />
-  </ContextProvider>,
+  </StrictMode>,
 )
