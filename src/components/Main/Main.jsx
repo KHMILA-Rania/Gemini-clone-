9029204e91b4d4fbed4dbcd00943f621ff4ee3a5
Rania KHMILA <rania.khmila@gmail.com>
2025-01-26 19:12:42 +0100

    almost there

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,13 @@
 
+import { useContext } from 'react'
 import { assets } from '../../assets/assets'
 import './Main.css'
+import '../../context/Context'
+import { Context } from '../../context/Context'
+
 const Main = () => {
+
+  const {onSent,recentPrompt,showResult,loading,resultData,setInput,input}=useContext(Context)
   return (
     <div className='main'>
       
@@ -12,8 +18,9 @@ const Main = () => {
 
 
       <div className="main-container">
-        
-        <div className="greed">
+
+        {!showResult ? <>
+          <div className="greed">
             <p> <span>Hello , ran</span></p>
             <p>How can I help you today ?</p>
         </div>
@@ -45,14 +52,37 @@ const Main = () => {
             </div>
 
         </div>
+        
+        </> : 
+        <div className='result'>
+          <div className="result-title">
+            <img src={assets.user_icon} alt="" />
+            <p>{recentPrompt}</p>
+          </div>
+
+          <div className="result-data">
+            <img src={assets.gemini_icon} alt="" />
+            {loading ? 
+            
+            <div className='loader'>
+              <hr />
+              <hr />
+              <hr />
+            </div> :
+            <p dangerouslySetInnerHTML={{__html:resultData}}></p>}
+          </div>
+        </div>
+        }
+        
+        
 
         <div className="main-bottom">
             <div className="search-box">
-                <input type='text' placeholder='Enter a promt here' />
+                <input onChange={(e)=>{setInput(e.target.value)}} value={input} type='text' placeholder='Enter a promt here' />
                 <div>
                     <img src={assets.gallery_icon} alt="" />
                     <img src={assets.mic_icon} alt="" />
-                    <img src={assets.send_icon} alt="" />
+                    <img onClick={()=>{onSent()}} src={assets.send_icon} alt="" />
                 </div>
                
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ContextProvider from './context/Context.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ContextProvider>
     <App />
-  </StrictMode>,
+  </ContextProvider>,
 )
